d28d8680ee00327ce1271f94672211641a55bd96
Duma_Tabby <tabithaondego9@gmail.com>
2026-03-08 11:03:51 -0700

    New

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import Fifthcomponent from './components/Fifthcomponent';
 import Firstcomponent from './components/Firstcomponent';
 import Fourthcomponent from './components/Fourthcomponent';
+import Notfound from './components/Notfound';
 import Secondcomponent from './components/Secondcomponent';
 import Sixthcomponent from './components/Sixthcomponent';
 import Thirdcomponent from './components/Thirdcomponent';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+
+        <nav>
+          <Link to="/first" className='navlinks'>First</Link>
+          <Link to="/second" className='navlinks'>Second</Link>
+          <Link to="/third" className='navlinks'>Third</Link>
+          <Link to="/fourth" className='navlinks'>Fourth</Link>
+          <Link to="/fifth" className='navlinks'>Fifth</Link>
+          <Link to="/Sixth" className='navlinks'>Sixth</Link>
+        </nav>
+
       <header className="App-header">
         
       <h1>Welcome to learning reactjs</h1>
@@ -17,13 +30,18 @@ function App() {
       
       </header>
       {/*Below we render/display/show our components */}
-      <Firstcomponent/>
-      <Secondcomponent/>
-      <Thirdcomponent/>
-      <Fourthcomponent/>
-      <Fifthcomponent/>
-      <Sixthcomponent/>
+      <Routes>
+        <Route path="/first" element={<Firstcomponent />}/>
+        <Route path="/second" element={<Secondcomponent/>}/>
+        <Route path="/third" element={<Thirdcomponent/>}/>
+        <Route path="/fourth" element={<Fourthcomponent/>}/>
+        <Route path="/fifth" element={<Fifthcomponent/>}/>
+        <Route path="/sixth" element={<Sixthcomponent/>}/>
+        <Route path='*'element={<Notfound/>}/>
+
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- src/components/Secondcomponent.jsx
+++ src/components/Secondcomponent.jsx
@@ -2,6 +2,7 @@ const Secondcomponent = ()=>{
     return(
         <div className="Secondcomponent">
             <h1>This is my second component</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, velit excepturi. Accusamus ipsa a dolore nisi, maiores, iusto possimus reiciendis necessitatibus quaerat aut quis? Ipsum voluptatibus blanditiis voluptatem totam. Quisquam.</p>
         </div>
     )
 }
